1e98185a6c5bd78f39b1384d1e33dd04c9eccb2c
mayurexh <mayuresh.pisat111@gmail.com>
2024-03-22 15:15:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+API_KEY = "6f84269b02fd43568d46ee0eb2ced6c7"
+URL = ("https://newsapi.org/v2/top-headlines?")
+
+
+def get_by_topHeadlines(category):
+    query_parameters = {
+        "apiKey": API_KEY,
+        "category": category,
+        "country": "in"
+    }
+    return get_articles(query_parameters)
+
+
+def get_articles(params):
+    r = requests.get(URL, params)
+    articles = r.json()['articles']
+
+    results =[]
+    for article in articles:
+        results.append({'title':article['title'], 'url': article['url'],'content':article['content']})
+
+    for result in results:
+        print(result['title'])
+        print(result['content'])
+        print(result['url'])
+        print(" ")
+
+
+if __name__ == "__main__":
+    get_by_topHeadlines("health")
+
